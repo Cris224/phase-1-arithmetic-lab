cbff4313da6fa78401ae09df14611ffc35a8bea4
Cristofer <cristoferlorenzana@gmail.com>
2021-10-12 19:40:36 -0500

    commit tree cfb4c697a939ca9c212e1fb038e5e724f5943632

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply =num1 * num2;
 
+const random = Math.ceil(Math.random()* 10)
+
+const num3 = 5;
+const num4 = 1;
+const mod = num3 - num4;
+
+const max = Math.max(0, 2, 20);
 // Write your code here
